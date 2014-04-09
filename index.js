963acff0b6c82a6e7d7c9d4f3d9b750050e2c0a5
Jeremie Miller <jeremie@jabber.org>
2014-04-09 16:11:44 -0600

    thjs path api refactor

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var os = require("os");
 
 exports.install = function(self, args)
 {
+  var paths = [];
   if(!args) args = {};
   if(typeof args.port != "number") args.port = 0;
 
@@ -14,8 +15,9 @@ exports.install = function(self, args)
   server.on("error", function(err){
     console.log("error from the UDP6 socket, dropping IPv6 support",err);
     self.deliver("ipv6",false);
-    delete self.paths.pub6;
-    delete self.paths.lan6;
+    paths.forEach(function(path){
+      self.pathSet(path,true);
+    });
   });
 
   self.deliver("ipv6",function(to,msg){
@@ -23,7 +25,6 @@ exports.install = function(self, args)
     server.send(buf, 0, buf.length, to.port, to.ip);    
   });
 
-  var networkIP = "";
   self.wait(true);
   server.bind(args.port, "::0", function(err){
     // regularly update w/ local ip address changes
@@ -31,14 +32,30 @@ exports.install = function(self, args)
     {
       var ifaces = os.networkInterfaces()
       var address = server.address();
+      var paths2 = [];
       for (var dev in ifaces) {
         ifaces[dev].forEach(function(details){
-          // upgrade to actual interface ip, prefer local ones
-          if(details.family == "IPv6" && !details.internal && self.isLocalIP(address.address)) address.address = details.address;
+          if(details.family != "IPv6") return;
+          if(details.internal) return;
+          if(details.address.length <= 4) return;
+          var path = {type:"ipv6",ip:details.address,port:address.port};
+          if(self.pathMatch(path,paths2)) return;
+          paths2.push(path);
         });
       }
-      networkIP = address.address; // used for local broadcasting
-      self.pathSet({type:"lan6",ip:address.address,port:address.port});
+      paths2.forEach(function(path){
+        var path1 = self.pathMatch(path,paths);
+        // add new ones
+        if(!path1) self.pathSet(path);
+        // look for old missing ones to remove
+        paths.splice(paths.indexOf(path1),1);
+      });
+      // leftovers gone
+      paths.forEach(function(path){
+        self.pathSet(path,true);
+      });
+      // for next time
+      paths = paths2;
       setTimeout(interfaces,10000);
     }
     interfaces();
